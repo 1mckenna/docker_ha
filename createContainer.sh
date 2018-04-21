d4c93f19f141d3a89a0d7772965ee58aa07155b2
1mckenna <mckenna.logan@gmail.com>
2018-04-21 18:21:50 -0500

    updated for new ha version

--- createContainer.sh
+++ createContainer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name hass --net=host --privileged --restart always --device /dev/ttyACM0:rwm -v /etc/localtime:/etc/localtime:ro -v /etc/fail2ban:/etc/fail2ban:rw -v /etc/letsencrypt:/etc/letsencrypt:ro -v /home/pi/docker_ha/homeassistant/configuration:/config lmckenna/rpi-home-assistant:0.67.0b0
+docker run -d --name hass --net=host --privileged --restart always --device /dev/ttyACM0:rwm -v /etc/localtime:/etc/localtime:ro -v /etc/fail2ban:/etc/fail2ban:rw -v /etc/letsencrypt:/etc/letsencrypt:ro -v /home/pi/docker_ha/homeassistant/configuration:/config lmckenna/rpi-home-assistant:0.67.1
